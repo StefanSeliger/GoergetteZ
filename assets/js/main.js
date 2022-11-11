053a56ef71a78aae484722d9a2e09ad56ff71bed
SuperCoder <supercoder@MBP-von-Stefan.local>
2022-11-11 16:05:37 +0100

    mobile menu

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+const openbtn = document.getElementsByClassName("openbtn")[0]
+
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+    openbtn.style.display = "none";
+}
+
+  /* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+    openbtn.style.display = "block";
+
+}
